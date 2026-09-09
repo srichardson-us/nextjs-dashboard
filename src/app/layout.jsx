f35442355dd563daf9b5afadcc0e3e6d7fa3b9e9
Scott Richardson <saricha3@gmail.com>
2026-09-09 18:06:59 -0400

    Initial commit

---
+++ src/app/layout.jsx
@@ -0,0 +1,15 @@
+import { inter } from '@/app/ui/fonts';
+import '@/app/ui/global.css';
+
+export const metadata = {
+  title: "MO-PCCO.R3 - NextJS Financial Dashboard",
+  description: "Starter code for Module 25 Capstone project - Financial Dashbard",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
